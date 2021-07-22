9fa651120b5f063cb04ec4553ab2155ab3532104
Chaitanya DA <58428344+chaitanya371@users.noreply.github.com>
2021-07-22 10:54:28 +0530

    Create production order - issue vs receipt.sql

---
+++ production order - issue vs receipt.sql
@@ -0,0 +1,21 @@
+/* query for production order - issue vs receipt */
+
+	Select 
+	T0.DocNum, 
+	T0.ItemCode, 
+	T2.ItemName, 
+	T1.ItemCode as 'Child Item',
+	T1.ItemName as 'Child Item Name',
+	T1.PlannedQty,
+	T1.IssuedQty,
+	T0.CmpltQty,
+	(T1.IssuedQty - T0.CmpltQty) as 'Difference'
+
+	From OWOR T0 
+	Inner Join WOR1 T1 on T1.DocEntry = T0.DocEntry 
+	Left Join OITM T2 on T2.ItemCode = T0.ItemCode 
+
+	Where 
+	--T0.Status = 'R' 
+	--and (T1.ItemCode like 'RC%%' or T1.ItemCode like 'PF%%')
+	(T1.IssuedQty - T0.CmpltQty) > 0
